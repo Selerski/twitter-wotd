69f16c73731b0bb3fb2463ecb71301e6d8e70fa2
Pawel <pawel.pietruszka99@gmail.com>
2020-04-05 23:03:01 +0200

    completed v 1.0

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
-// const app = require('express')();
-// var io = require('socket.io')(app);
-// var cfg = require('./config.json');
-// var tw = require('node-tweet-stream')(cfg);
-
 // tw.track('socket.io');
 // tw.track('javascript');
-
-const app = require('express')();
+const express = require('express');
+const app = express();
+const http = require('http');
+const socketio = require('socket.io');
+const bodyParser = require('body-parser');
 const path = require('path');
-const http = require('http').createServer(app);
-const Twitter = require('node-tweet-stream')();
-Twitter.track('kardashian');
+const cors = require('cors');
+const server = http.createServer(app);
+const io = socketio(server);
 
-app.get('/', function (req, res) {
+const Twitter = require('twitter');
+require('dotenv').config();
+
+const port = process.env.PORT || 3001;
+const { API_KEY, API_SECRET_KEY, ACC_TOKEN, ACC_TOKEN_SECRET } = process.env;
+
+const t = new Twitter({
+  consumer_key: API_KEY,
+  consumer_secret: API_SECRET_KEY,
+  access_token_key: ACC_TOKEN,
+  access_token_secret: ACC_TOKEN_SECRET,
+});
+
+app.use(bodyParser.json());
+app.use(cors({origin: 'https://localhost:3001'}));
+
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-tw.on('tweet', function (tweet) {
-  io.emit('tweet', tweet);
+io.on('connection', (socket) => {
+  t.stream('statuses/filter', { track: 'java' }, (stream) => {
+    stream.on('data', ({ text }) => {
+      socket.emit('tweet', { tweet: text });
+    });
+
+    stream.on('error', function (error) {
+      throw error;
+    });
+  });
 });
 
-http.listen(3000, function () {
+server.listen(port, function () {
   console.log('listening on *:3000');
 });
